7cd05ece43029316345172dfd2d86c419ae832f1
vradchenko <vradchenko@callpanda.com>
2022-06-14 01:06:33 +0300

    added react to project

--- app/main.py
+++ app/main.py
@@ -1,30 +1,35 @@
 import logging
 
 from fastapi import Depends, FastAPI
-from starlette.responses import RedirectResponse
-
+from starlette.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, RedirectResponse
 from app.dependencies import auth_as_user
 from app.exceptions import NotAuthorizedException
 from app.routers import editor, login
 from app.schemes.twitch import TwitchUser
+
+logger = logging.getLogger()
+
 
 app = FastAPI()
 
 app.include_router(login.router)
 app.include_router(editor.router)
 
-
-logger = logging.getLogger()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
 @app.exception_handler(NotAuthorizedException)
-async def not_authorized_redirect(
-    *_,
-    **__,
-) -> RedirectResponse:
+async def not_authorized_redirect(*_, **__) -> RedirectResponse:
     return RedirectResponse(url="/login")
 
 
-@app.get("/")
-async def root(auth_user: TwitchUser = Depends(auth_as_user)):
-    return auth_user.dict()
+@app.get("/", response_class=HTMLResponse)
+async def root(_: TwitchUser = Depends(auth_as_user)):
+    with open('app/static/index.html', 'r') as index_html:
+        html_response = index_html.read()
+    return HTMLResponse(
+        content=html_response,
+        status_code=200,
+        media_type="text/html",
+    )
